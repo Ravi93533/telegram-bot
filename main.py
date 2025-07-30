b3f382245d2c7f5d4644c3c8d2281b7bb0ee1b8f
Ravi93533 <ranirani9353@gmail.com>
2025-07-30 23:44:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -364,9 +364,9 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     reply_markup = InlineKeyboardMarkup(keyboard)
     await update.message.reply_text(
         "Salom👋\n"
-        "Men reklamalarni, ssilkalani guruhlarda o‘chirib beraman, profilingiz ID gizni aniqlab beraman, guruxingizga majbur odam qo'shib beraman va majbur kanalingizga a'zo qildiraman va boshqa ko'plab yordamlar beraman 👨🏻‍✈\n\n\n\n"
-        "Bot komandalari qo'llanmasi 👉 /help
-        "Faqat Ishlashim uchun guruhingizga qo‘shib, admin berishingiz kerak 🙂",
+        "Men reklamalarni, ssilkalani guruhlarda o‘chirib beraman, profilingiz ID gizni aniqlab beraman, guruxingizga majbur odam qo'shib beraman va majbur kanalingizga a'zo qildiraman va boshqa ko'plab yordamlar beraman 👨🏻‍✈\n\n"
+        "Bot komandalari qo'llanmasi 👉 /help\n"
+        "Faqat Ishlashim uchun guruhingizga qo‘shib, admin berishingiz kerak 🙂\n\n"
         "Murojaat uchun👉 @Devona1995",
         reply_markup=reply_markup
     )
